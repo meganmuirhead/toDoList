7a2d1fa5e36f597badbff8babe11fc7cc6b48ba1
meganmuirhead <megan@s56.net>
2018-03-19 07:42:46 -0600

    playing with modal

---
+++ action.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="action.js" defer></script>
+</head>
+<body>
+<div class="container">
+
+    <div class="headbar">
+    <div class="left">
+    Megan Muirhead
+</div>
+<div class="right">
+    right side for now
+                   </div>
+                   </div>
+                   <div class="main">
+    <div class="header">
+    <div class="headLeft">
+    To do list
+</div>
+<div class="headerRight">
+    <a href="#" class="blueBtn">Add List</a>
+<a href="#" class="blueBtn">Add Item</a>
+<a href="#" class="blueBtn">Clear Complete Tasks</a>
+
+</div>
+</div>
+<div class="list">
+    <div class="toDoList">
+    <span class="listLabel">List label</span>
+</div>
+</div>
+</div>
+
+</div>
+
+</body>
+</html>
