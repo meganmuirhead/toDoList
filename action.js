6345283c3ab51cbb93261854565b856c4f74fd0d
meganmuirhead <megan@s56.net>
2018-03-31 09:46:53 -0600

    added li

--- action.js
+++ action.js
@@ -21,9 +21,10 @@ function newCategory() {
    CDiv.append(h3);
    Bdiv.append(CDiv);
    var myItem = document.createElement("input");
+   $(myItem).addClass("inputCDiv");
    $(myItem).attr("placeholder", "Add item");
    CDiv.append(myItem);
-   var CDivspan = document.createElement("span");
+   var CDivspan = document.createElement("button");
    $(CDivspan).addClass("addBtn");
    $(CDivspan).attr("onclick", "newTodo(this)");
    CDiv.append(CDivspan);
@@ -33,6 +34,7 @@ function newCategory() {
 
    Bdiv.append(DDDiv);
    DDDiv.append(myDDDivUl);
+
 
 }
 
@@ -63,34 +65,73 @@ function removeRow(theElement) {
         });
 }
 
+
 // Create an item on the todo list
 function newTodo(buttonElement) {
     var li = document.createElement("li");
-    var CDiv = buttonElement.parent();
+    var CDiv = $(buttonElement).parent();
 
-    var inputPlaceholder = $(CDiv).find("input.CDiv");
+    var input = $(CDiv).find("input.inputCDiv");
+    var inputPlaceholder = input[0].value;
+    var t = document.createTextNode(inputPlaceholder);
 
+    li.append(t);
+    var trashDiv = document.createElement("div");
+    li.append(trashDiv);
+    $(trashDiv).addClass("fa");
+    $(trashDiv).addClass("fa-trash");
+    $(trashDiv).attr("onclick", "removeRow(this)");
 
-    var inputValue = document.getElementById("myNewItem").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myNewItem").value = "";
+    var iconspotDiv = document.createElement("div");
+    li.append(iconspotDiv);
+    $(iconspotDiv).addClass("iconspot");
+    $(iconspotDiv).addClass("fa");
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    var buttz = document.createElement("button");
+    li.append(buttz);
+    var buttzText = document.createTextNode("Complete");
+    buttz.append(buttzText);
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+    $(buttz).attr("onclick", "markComplete(this)");
+
+    var buttz2 = document.createElement("button");
+    li.append(buttz2);
+    var buttz2Text = document.createTextNode("Edit");
+    buttz2.append(buttz2Text);
+    $(buttz).attr("onclick", "edit(this)");
+
+    var BDiv = $(CDiv).parent();
+    var returnUl = $(BDiv).find("ul");
+
+    returnUl.append(li);
+
 }
+
+
+
+
+
+
+
+
+
+    //
+    // if (inputValue === '') {
+    //     alert("You must write something!");
+    // } else {
+    //     document.getElementById("myUL").appendChild(li);
+    // }
+    // document.getElementById("myNewItem").value = "";
+    //
+    // var span = document.createElement("SPAN");
+    // var txt = document.createTextNode("\u00D7");
+    // span.className = "close";
+    // span.appendChild(txt);
+    // li.appendChild(span);
+    //
+    // for (i = 0; i < close.length; i++) {
+    //     close[i].onclick = function() {
+    //         var div = this.parentElement;
+    //         div.style.display = "none";
+    //     }
+    // }
